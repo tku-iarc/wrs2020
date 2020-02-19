ffcc014b48906452a16650520bea1082fe3e3f1f
NEET <zxc455233@gmail.com>
2020-02-19 23:16:25 +0800

    First mir_bridge

---
+++ strategy/scripts/core.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+import sys
+import math
+from std_msgs.msg import String
+from my_state_machine import MyStateMachine
+import dynamic_reconfigure.client
+
+class Strategy(object):
+  def __init__(self, sim=False):
+    rospy.init_node('core', anonymous=True)
+    self.rate = rospy.Rate(200)
+    self.robot = MyStateMachine(sim)
+    self.main()
+
+  def main(self):
+    while not rospy.is_shutdown():
+      if not self.robot.start:
+        self.robot.toIdle()
+      else:
+        pass
+
+        if rospy.is_shutdown():
+          break
+
+        ## Keep Current State Running
+        keepState = 'to' + self.robot.current_state.name
+        getattr(self.robot, keepState)
+
+        self.rate.sleep()
+
+if __name__ == '__main__':
+  try:
+      s = Strategy(True) # True is simulated mode
+  except rospy.ROSInterruptException:
+    pass 
